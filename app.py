c442ea500c3d047b1d01e33b38142bd4eccafd4d
dbasu03 <116761602+dbasu03@users.noreply.github.com>
2024-08-13 00:32:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer,util
 import faiss
 import numpy as np
 
